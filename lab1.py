82368711979e7a44a02e39eb263ed80dd57ec047
diegoteran99 <diegoteranbaselli@gmail.com>
2021-03-25 15:31:26 -0300

    second commit

--- lab1.py
+++ lab1.py
@@ -1,5 +1,9 @@
 import random
+cartas=[]
 cant_cartas = int(input("Ingrese la cantidad de cartas:"))
-cartas = 2*list(range(1, cant_cartas+1))
-random.shuffle(cartas)
+for i in range(2):
+    cartas.append([])
+    for j in range(1, cant_cartas+1):
+        cartas[i].append(j)
+        random.shuffle(cartas[i])
 print(cartas)

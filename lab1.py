15c8b46ce3f366ef3c717606419ecdb9bb490d3f
diegoteran99 <diegoteranbaselli@gmail.com>
2021-03-25 17:31:00 -0300

    fourth commit

--- lab1.py
+++ lab1.py
@@ -1,4 +1,14 @@
 import random
+
+def crear_tablero(cant_cartas):
+    tablero=[]
+    for i in range(2):
+        tablero.append([])
+        for j in range(1,cant_cartas+1):
+            tablero[i].append("▓")
+    print(tablero)
+
+
 cartas=[]
 cant_cartas = int(input("Ingrese la cantidad de cartas:"))
 for i in range(2):
@@ -7,3 +17,5 @@ for i in range(2):
         cartas[i].append(j)
         random.shuffle(cartas[i])
 print(cartas)
+
+crear_tablero(cant_cartas)

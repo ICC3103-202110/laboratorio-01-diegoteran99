516c3b77b24f263da896694f8ae891e4661b9e38
diegoteran99 <diegoteranbaselli@gmail.com>
2021-03-26 16:23:53 -0300

    final commit

--- lab1.py
+++ lab1.py
@@ -11,7 +11,7 @@ def numbers(cards):
 def board(card_list):
     board = []
     k = 0
-    for i in range(0,2):
+    for i in range(2):
         row = []
         for j in range(0,int(len(card_list)/2)):
             row.append(card_list[k])
@@ -41,18 +41,18 @@ def covered_board(board):
         covered_board.append(row2)
     return covered_board
 
-def coord(C,R,covered_board,board):
-    covered_board[C][R] = board[C][R]
+def coord(R,C,covered_board,board):
+    covered_board[R][C] = board[R][C]
     board_censor(covered_board)
-    return board[C][R]
+    return board[R][C]
 
-def clean(first_C,first_R,second_C,second_R):
-    covered_board[first_C][first_R]="▓"
-    covered_board[second_C][second_R]="▓"
+def clean(first_R,first_C,second_R,second_C):
+    covered_board[first_R][first_C]="▓"
+    covered_board[second_R][second_C]="▓"
 
-def erase_cards(first_C,first_R,second_C,second_R):
-    covered_board[first_C][first_R]=" "
-    covered_board[second_C][second_R]=" "
+def erase_cards(first_R,first_C,second_R,second_C):
+    covered_board[first_R][first_C]=" "
+    covered_board[second_R][second_C]=" "
     
 def print_board(board): 
     for i in board:
@@ -68,8 +68,8 @@ def change_player(player):
         player=1
     return player
 
-def valid(C,R,board):
-    if C<0 or C>1 or R<0 or C>len(board) or R<0 or R>len(board[0]) or board[C][R]!="▓":
+def valid(R,C,board):
+    if R<0 or R>1 or C<0 or R>len(board) or C>len(board[0]) or board[R][C]!="▓":
         print("Not valid\n")
         return False
     return True
@@ -87,13 +87,7 @@ print()
 card_list = numbers(cards)
 board = board(card_list)
 covered_board = covered_board(board)
-#print(covered_board)
-#print("board")
-print_board(board)
-#print("censored board")
 board_censor(covered_board)
-
-#-------------------------------------------------------------------------------
 p1_points = 0
 p2_points = 0
 player=1
@@ -101,35 +95,37 @@ player=1
 while count(covered_board) > 0:
     print("\nNow it's playing Player",player)
     print()
-    move1_C = int(input("Select a column (0 or 1): "))
-    move1_R = int(input("Select a row (0,1,2,3,etc): "))
+
+    move1_R = int(input("Select a row (0 or 1): "))
+    move1_C = int(input("Select a column (0,1,2,3,etc): "))
     print()
-    while valid(move1_C,move1_R,covered_board) == False:
-        move1_C = int(input("Select a column (0 or 1): "))
-        move1_R = int(input("Select a row (0,1,2,3,etc): "))
+    while valid(move1_R,move1_C,covered_board) == False:
+        move1_R = int(input("Select a row (0 or 1): "))
+        move1_C = int(input("Select a column (0,1,2,3,etc): "))
         print()
-    card1 = coord(move1_C, move1_R, covered_board, board)
+    card1 = coord(move1_R, move1_C, covered_board, board)
     print()
-    move2_C = int(input("Select a column (0 or 1): "))
-    move2_R = int(input("Select a row (0,1,2,3,etc): "))
+
+    move2_R = int(input("Select a row (0 or 1): "))
+    move2_C = int(input("Select a column (0,1,2,3,etc): "))
     print()
-    while valid(move2_C,move2_R,covered_board) == False:
-        move2_C = int(input("Select a column (0 or 1): "))
-        move2_R = int(input("Select a row (0,1,2,3,etc): "))
+    while valid(move2_R,move2_C,covered_board) == False:
+        move2_R = int(input("Select a row (0 or 1): "))
+        move2_C = int(input("Select a column (0,1,2,3,etc): "))
         print()
-    card2 =coord(move2_C,move2_R, covered_board, board)
+    card2 =coord(move2_R,move2_C, covered_board, board)
     print()
     
     if card1 == card2:
         print("It's correct, keep playing!")
-        erase_cards(move1_C,move1_R,move2_C,move2_R)
+        erase_cards(move1_R,move1_C,move2_R,move2_C)
         if player==1:
             p1_points += 1
         else:
             p2_points += 1
     else:
         print("Wrong\n")
-        clean(move1_C,move1_R,move2_C,move2_R)
+        clean(move1_R,move1_C,move2_R,move2_C)
         board_censor(covered_board)
         player = change_player(player)
 
@@ -138,4 +134,4 @@ if p1_points > p2_points:
 elif p1_points < p2_points:
     print("The winner is Player 2 with", p2_points, "pairs")
 else:
-    print("It´s a tie")
+    print("It´s a tie, both player made", p1_points, "pairs")
